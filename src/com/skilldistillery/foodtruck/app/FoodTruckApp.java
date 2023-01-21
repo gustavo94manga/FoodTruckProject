772053f46fa167054fa1cc8ce4e46f938f95a7c4
Angel Garza <gustavo94manga@gmail.com>
2023-01-21 12:33:47 -0600

    saving project as a checkpoint

--- src/com/skilldistillery/foodtruck/app/FoodTruckApp.java
+++ src/com/skilldistillery/foodtruck/app/FoodTruckApp.java
@@ -1,16 +1,83 @@
 package com.skilldistillery.foodtruck.app;
 
+import java.util.Scanner;
+
 import com.skilldistillery.foodtruck.entities.Foodtruck;
 
 public class FoodTruckApp {
-	
-	//fleet of Foodtrucks
-	private Foodtruck[] fleet;
-	
+	// This main food truck app does NOT pass in the id value for any foodtruck
+	// fleet of Foodtrucks
+	private Foodtruck[] fleet = new Foodtruck[MAX_FLEET];
+	private static int MAX_FLEET = 5;
+	private static int numOfFoodTruck = 0;
+	private Scanner customer = new Scanner(System.in);
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		FoodTruckApp ftApp = new FoodTruckApp();
+		ftApp.truckInput();
+		ftApp.menuOptions();
 		
+
 	}
 
+
+	public void truckInput() {
+		String name;
+		String foodType;
+		int rating;
+
+		for (int i = 0; numOfFoodTruck < MAX_FLEET; i++) {
+			System.out.println("Enter the name of the food truck OR type quit to continue: ");
+			name = customer.nextLine();
+			if (name.equalsIgnoreCase("quit")) {
+				
+				break;
+			}
+
+			System.out.println("Enter the food type: ");
+			foodType = customer.nextLine();
+			System.out.println("Enter your rating for the truck from 1 to 5: ");
+			rating = customer.nextInt();
+			customer.nextLine();
+			Foodtruck truck = new Foodtruck(name, foodType, rating);
+			fleet[i] = truck;
+		}
+	}
+
+	public void menuOptions() {
+		String option;
+		boolean running = true;
+		do {
+			System.out.println("Please choose a number from the Menu:");
+			System.out.println("---------------MENU---------------");
+			System.out.println("1. List all existing food trucks");
+			System.out.println("2. See the average rating of food trucks");
+			System.out.println("3. Display the highest rated food truck");
+			System.out.println("4. quit");
+			
+			option = customer.nextLine(); 
+			
+			switch (option) {
+			case "1":
+				System.out.println();
+				break;
+			case "2":
+				System.out.println("The food trucks' combined average ratings are: ");
+				break;
+			case "3":
+				System.out.println("The Highest rated food truck is: ");		//add \n (newlines) for each
+				break;
+			case "4":
+				System.out.println("Thank you for your input, have a nice day");
+				running = false;
+				break;
+
+			default:
+				System.err.println("Error: Please enter an appropriate option.");
+
+			}
+
+		} while (running);
+		customer.close();
+	}
 }
--- src/com/skilldistillery/foodtruck/entities/Foodtruck.java
+++ src/com/skilldistillery/foodtruck/entities/Foodtruck.java
@@ -2,5 +2,56 @@ package com.skilldistillery.foodtruck.entities;
 
 public class Foodtruck {
 	private static int nextTruckId;
-	private int id;
+	private int id = 0;
+	private String name;
+	private String foodType;
+	private int rating;
+	
+	
+	public Foodtruck() {
+		
+	}
+	public Foodtruck(String name, String foodType, int rating) {
+		
+		this.id = id + 1;
+		this.name = name;
+		this.foodType = foodType;
+		this.rating = rating;
+	}
+	public static int getNextTruckId() {
+		return nextTruckId;
+	}
+	public static void setNextTruckId(int nextTruckId) {
+		Foodtruck.nextTruckId = nextTruckId;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getFoodType() {
+		return foodType;
+	}
+	public void setFoodType(String foodType) {
+		this.foodType = foodType;
+	}
+	public int getRating() {
+		return rating;
+	}
+	public void setRating(int rating) {
+		this.rating = rating;
+	}
+	@Override
+	public String toString() {
+		return "Foodtruck id: " + id + ", name: " + name + ", foodType: " + foodType + ", rating: " + rating + "";
+	}
+	
+	
 }
