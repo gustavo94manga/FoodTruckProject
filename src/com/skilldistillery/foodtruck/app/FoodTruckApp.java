43b697581baae9d4232470fb99260cfd6c47b90f
Angel Garza <gustavo94manga@gmail.com>
2023-01-20 18:08:24 -0600

    initial commit

---
+++ src/com/skilldistillery/foodtruck/app/FoodTruckApp.java
@@ -0,0 +1,16 @@
+package com.skilldistillery.foodtruck.app;
+
+import com.skilldistillery.foodtruck.entities.Foodtruck;
+
+public class FoodTruckApp {
+	
+	//fleet of Foodtrucks
+	private Foodtruck[] fleet;
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
