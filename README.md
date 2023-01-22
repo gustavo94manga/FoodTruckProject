7f19eaf590ad0a4af33a5391f4b63609c7b204a2
Angel Garza <gustavo94manga@gmail.com>
2023-01-21 20:50:41 -0600

    finished project

--- README.md
+++ README.md
@@ -1,7 +1,11 @@
 # FoodTruck Application
+Many users often leave reviews for the places they frequent. This application is a small sample of how a customer would leave a review for a mobile restaurant, in other words, a food truck.
 
 # Description
+This Food Truck Project is designed to show a unique ID identifier for each Food Truck. The user is not able to manually enter and set an ID, as that would be considered sensitive information. The user gets to interact with this application by entering the name, the type of food it serves, and by giving it an overall rating. Similar to an online review, the user will then get that same information output back to them, again, with the unique ID already assigned to each truck. The user is able to loop through the menu options after the data is entered, and continue choosing to see the different types of information display. 
 
 # Technologies Used
+Java, Eclipse
 
 # Lessons Learned
+I got more hands on experience working with constructors, static and non-static methods, switch statements, public and private fields, as well as with single dimension arrays. I had to make proper use of for Loops in order to go through the array indexes and get the average ratings, as well as the highest single rating given by the user. 
--- src/com/skilldistillery/foodtruck/app/FoodTruckApp.java
+++ src/com/skilldistillery/foodtruck/app/FoodTruckApp.java
@@ -11,15 +11,15 @@ public class FoodTruckApp {
 	private static int MAX_FLEET = 5;
 	private static int numOfFoodTruck = 0;
 	private Scanner customer = new Scanner(System.in);
+	private int counter = 0;
 
 	public static void main(String[] args) {
 		FoodTruckApp ftApp = new FoodTruckApp();
+
 		ftApp.truckInput();
 		ftApp.menuOptions();
-		
 
 	}
-
 
 	public void truckInput() {
 		String name;
@@ -30,7 +30,7 @@ public class FoodTruckApp {
 			System.out.println("Enter the name of the food truck OR type quit to continue: ");
 			name = customer.nextLine();
 			if (name.equalsIgnoreCase("quit")) {
-				
+
 				break;
 			}
 
@@ -41,12 +41,13 @@ public class FoodTruckApp {
 			customer.nextLine();
 			Foodtruck truck = new Foodtruck(name, foodType, rating);
 			fleet[i] = truck;
+			counter++;
 		}
 	}
 
 	public void menuOptions() {
 		String option;
-		boolean running = true;
+		boolean running = true; //
 		do {
 			System.out.println("Please choose a number from the Menu:");
 			System.out.println("---------------MENU---------------");
@@ -54,26 +55,50 @@ public class FoodTruckApp {
 			System.out.println("2. See the average rating of food trucks");
 			System.out.println("3. Display the highest rated food truck");
 			System.out.println("4. quit");
-			
-			option = customer.nextLine(); 
-			
+
+			option = customer.nextLine();
+
 			switch (option) {
 			case "1":
-				System.out.println();
+				for (int i = 0; i < counter; i++) {
+					System.out.println("The Truck ID is: " + fleet[i].getId());
+					System.out.println("The food truck name is: " + fleet[i].getName());
+					System.out.println("The food type is: " + fleet[i].getFoodType());
+				}
+				System.out.println("Choose another option to see the rating \n");
 				break;
+
 			case "2":
-				System.out.println("The food trucks' combined average ratings are: ");
+				int sum = 0;
+				double avg = 0.0;
+				for (int i = 0; i < counter; i++) {
+					sum += fleet[i].getRating();
+				}
+				avg = sum / counter;
+				System.out.println("The food trucks' combined average ratings are: " + avg + "\n");
 				break;
+
 			case "3":
-				System.out.println("The Highest rated food truck is: ");		//add \n (newlines) for each
+				int highest = 0;
+				Foodtruck highestNumber = fleet[0];
+				for (int i = 0; i < counter; i++) {
+					Foodtruck highestAvg = fleet[i];
+					if (highestAvg.getRating() > highest) {
+						highest = highestAvg.getRating();
+						highestNumber = highestAvg;
+					}
+				}
+				System.out.println("The Highest rated food truck is: " + highestNumber + "\n"); // add \n (newlines) for
+																								// each
 				break;
+
 			case "4":
 				System.out.println("Thank you for your input, have a nice day");
 				running = false;
 				break;
 
 			default:
-				System.err.println("Error: Please enter an appropriate option.");
+				System.err.println("Error: Please enter an appropriate option. \n");
 
 			}
 
